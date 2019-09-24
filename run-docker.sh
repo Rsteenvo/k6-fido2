47bceaf0f931b7f0f3d48ce5776c85618973d64a
Jim Dunkerton <jamesd@backbase.com>
2019-09-24 08:04:57 +0100

    Always restart Docker container. Use applicable version of maven-release-plugin.

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -d -p 8877:8080 --name device-services-test-crypto-service device-services-test-crypto-service:latest
+docker run -d -p 8877:8080 --restart=always --name device-services-test-crypto-service device-services-test-crypto-service:latest
