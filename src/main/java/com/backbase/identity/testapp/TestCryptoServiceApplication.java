4b260bcb98fbed839f056fbb6b7dae4af62fa759
Alex Richardson <alexanderr@backbase.com>
2022-08-03 09:07:06 +0100

    DV-2884 Upgrade to Java 17

--- src/main/java/com/backbase/identity/testapp/TestCryptoServiceApplication.java
+++ src/main/java/com/backbase/identity/testapp/TestCryptoServiceApplication.java
@@ -5,7 +5,9 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class TestCryptoServiceApplication extends SpringBootServletInitializer {
 
